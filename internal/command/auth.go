a76de15894383ae4c97a0bb979981654661486b8
Charlie Egan <charlieegan3@users.noreply.github.com>
2022-09-13 10:44:00 +0100

    Add auth status command
    $ go run main.go auth status
    Token path: /Users/charlieegan/Library/Application Support/jsctl/token.json
    Logged in as: charlie.egan@jetstack.io
    Current Organization: jetstack
    
    $ go run main.go auth status --credentials=creds.json
    Logged in as: example3661206251@jetstack.platform.jetstack.io
    Current Organization: jetstack
    
    Signed-off-by: Charlie Egan <charlieegan3@users.noreply.github.com>

--- internal/command/auth.go
+++ internal/command/auth.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jetstack/jsctl/internal/auth"
-	"github.com/jetstack/jsctl/internal/config"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/spf13/cobra"
 	"github.com/toqueteos/webbrowser"
 	"golang.org/x/oauth2"
+
+	"github.com/jetstack/jsctl/internal/auth"
+	"github.com/jetstack/jsctl/internal/config"
 )
 
 // Auth returns a cobra.Command instance that is the root for all "jsctl auth" subcommands.
@@ -23,6 +25,70 @@ func Auth() *cobra.Command {
 	cmd.AddCommand(
 		authLogin(),
 		authLogout(),
+		authStatus(),
+	)
+
+	return cmd
+}
+
+func authStatus() *cobra.Command {
+	var credentials string
+
+	cmd := &cobra.Command{
+		Use:   "status",
+		Short: "Print the logged in account and token location",
+		Run: run(func(ctx context.Context, args []string) error {
+			var token *oauth2.Token
+			var err error
+			var tokenPath string
+			if credentials != "" {
+				tokenPath = credentials
+				token, err = loginWithCredentials(ctx, auth.GetOAuthConfig(), credentials)
+				if err != nil {
+					return fmt.Errorf("failed to login with credentials file %q: %w", credentials, err)
+				}
+			} else {
+				tokenPath, err = auth.DetermineTokenFilePath()
+				if err != nil {
+					fmt.Println("Can't find token", err)
+				}
+				fmt.Println("Token path:", tokenPath)
+
+				token, err = auth.LoadOAuthToken()
+				if err != nil {
+					fmt.Println("Not logged in")
+					return nil
+				}
+			}
+
+			claims := jwt.MapClaims{}
+			_, err = jwt.ParseWithClaims(token.AccessToken, claims, func(token *jwt.Token) (interface{}, error) {
+				return []byte(""), nil
+			})
+
+			email, ok := claims["https://jetstack.io/claims/name"].(string)
+			if ok {
+				fmt.Println("Logged in as:", email)
+			}
+
+			cnf, ok := config.FromContext(ctx)
+			if !ok || cnf.Organization == "" {
+				fmt.Println("You do not have an organization selected, select one using: \n\n\tjsctl config set organization [name]\n\n" +
+					"To view organizations you have access to, list them using: \n\n\tjsctl organizations list")
+				return nil
+			}
+			fmt.Println("Current Organization:", cnf.Organization)
+
+			return nil
+		}),
+	}
+
+	flags := cmd.PersistentFlags()
+	flags.StringVar(
+		&credentials,
+		"credentials",
+		os.Getenv("JSCTL_CREDENTIALS"),
+		"The location of a credentials file to use instead of the normal oauth login flow",
 	)
 
 	return cmd
