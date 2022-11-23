e25d298cbb5994c1074d2cb39b6b3b5af0c70336
Charlie Egan <charlieegan3@users.noreply.github.com>
2022-11-23 10:33:57 +0000

    Support restoring vei issuers

--- internal/kubernetes/restore/restore.go
+++ internal/kubernetes/restore/restore.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	veiv1alpha1 "github.com/jetstack/venafi-enhanced-issuer/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/jetstack/jsctl/internal/kubernetes/yaml"
@@ -16,6 +17,8 @@ import (
 type RestoredIssuers struct {
 	CertManagerIssuers        []*certmanagerv1.Issuer
 	CertManagerClusterIssuers []*certmanagerv1.ClusterIssuer
+	VenafiIssuers             []*veiv1alpha1.VenafiIssuer
+	VenafiClusterIssuers      []*veiv1alpha1.VenafiClusterIssuer
 
 	MissedIssuers []string
 }
@@ -34,33 +37,54 @@ func ExtractOperatorManageableIssuersFromBackupFile(backupFilePath string) (*Res
 	}
 
 	for _, resource := range resources {
-		if resource.GetAPIVersion() != "cert-manager.io/v1" {
-			if strings.Contains(resource.GetKind(), "Issuer") {
+
+		switch resource.GroupVersionKind().Group {
+		case "cert-manager.io":
+			switch resource.GroupVersionKind().Kind {
+			case "Issuer":
+				var issuer *certmanagerv1.Issuer
+
+				err = runtime.DefaultUnstructuredConverter.FromUnstructured(resource.Object, &issuer)
+				if err != nil {
+					return nil, fmt.Errorf("failed to convert unstructured to cert-manager.io/v1 Issuer: %w", err)
+				}
+
+				restoredIssuers.CertManagerIssuers = append(restoredIssuers.CertManagerIssuers, issuer)
+			case "ClusterIssuer":
+				var issuer *certmanagerv1.ClusterIssuer
+
+				err = runtime.DefaultUnstructuredConverter.FromUnstructured(resource.Object, &issuer)
+				if err != nil {
+					return nil, fmt.Errorf("ailed to convert unstructured to cert-manager.io/v1 ClusterIssuer: %w", err)
+				}
+
+				restoredIssuers.CertManagerClusterIssuers = append(restoredIssuers.CertManagerClusterIssuers, issuer)
+			}
+		case "jetstack.io":
+			switch resource.GroupVersionKind().Kind {
+			case "VenafiIssuer":
+				var issuer *veiv1alpha1.VenafiIssuer
+
+				err = runtime.DefaultUnstructuredConverter.FromUnstructured(resource.Object, &issuer)
+				if err != nil {
+					return nil, fmt.Errorf("failed to convert unstructured to Venafi Issuer: %w", err)
+				}
+
+				restoredIssuers.VenafiIssuers = append(restoredIssuers.VenafiIssuers, issuer)
+			case "VenafiClusterIssuer":
+				var issuer *veiv1alpha1.VenafiClusterIssuer
+
+				err = runtime.DefaultUnstructuredConverter.FromUnstructured(resource.Object, &issuer)
+				if err != nil {
+					return nil, fmt.Errorf("failed to convert unstructured to Venafi Cluster Issuer: %w", err)
+				}
+
+				restoredIssuers.VenafiClusterIssuers = append(restoredIssuers.VenafiClusterIssuers, issuer)
+			}
+		default:
+			if strings.Contains(resource.GroupVersionKind().Kind, "Issuer") {
 				restoredIssuers.MissedIssuers = append(restoredIssuers.MissedIssuers, fmt.Sprintf("%s/%s", resource.GetKind(), resource.GetName()))
 			}
-			continue
-		}
-
-		if resource.GetKind() == "Issuer" {
-			var issuer *certmanagerv1.Issuer
-
-			err = runtime.DefaultUnstructuredConverter.FromUnstructured(resource.Object, &issuer)
-			if err != nil {
-				return nil, fmt.Errorf("failed to convert unstructured to cert-manager.io/v1 Issuer: %w", err)
-			}
-
-			restoredIssuers.CertManagerIssuers = append(restoredIssuers.CertManagerIssuers, issuer)
-		}
-
-		if resource.GetKind() == "ClusterIssuer" {
-			var issuer *certmanagerv1.ClusterIssuer
-
-			err = runtime.DefaultUnstructuredConverter.FromUnstructured(resource.Object, &issuer)
-			if err != nil {
-				return nil, fmt.Errorf("ailed to convert unstructured to cert-manager.io/v1 ClusterIssuer: %w", err)
-			}
-
-			restoredIssuers.CertManagerClusterIssuers = append(restoredIssuers.CertManagerClusterIssuers, issuer)
 		}
 	}
 
