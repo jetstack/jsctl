b7fb347d34f900d730c9ad36fc4690da3174fccb
Charlie Egan <charlieegan3@users.noreply.github.com>
2022-11-25 11:35:14 +0000

    Improve command output

--- internal/command/clusters/uninstall.go
+++ internal/command/clusters/uninstall.go
@@ -31,15 +31,20 @@ func verify(run types.RunFunc, kubeConfigPath string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "verify",
 		Short: "Check that a cluster is ready to have Jetstack Software uninstalled",
-		Args:  cobra.MatchAll(cobra.ExactArgs(0)),
+		Long: `Runs the following checks:
+* Checks secrets containing certificates are safe from garbage collection
+* Checks for any upcoming renewals
+* Checks for certificates currently being issued
+* Checks for stuck certificate requests
+`,
+		Args: cobra.MatchAll(cobra.ExactArgs(0)),
 		Run: run(func(ctx context.Context, args []string) error {
 			kubeCfg, err := kubernetes.NewConfig(kubeConfigPath)
 			if err != nil {
 				return err
 			}
 
-			// first, we need to check that secrets containing certs are safe from
-			// garbage collection
+			fmt.Fprintf(os.Stdout, "Checking certificates are safe from garbage collection...\n")
 			messagesOwnedSecrets := []string{}
 			secretsClient, err := clients.NewGenericClient[*corev1.Secret, *corev1.SecretList](
 				&clients.GenericClientOptions{
@@ -70,7 +75,7 @@ func verify(run types.RunFunc, kubeConfigPath string) *cobra.Command {
 				}
 			}
 
-			// next, check if any certificates are about to be renewed
+			fmt.Fprintf(os.Stdout, "Checking for upcoming certificate renewals...\n")
 			messagesUpcomingRenewals := []string{}
 			upcomingCerts := [][]string{}
 			certificateClient, err := clients.NewCertificateClient(kubeCfg)
@@ -109,7 +114,7 @@ func verify(run types.RunFunc, kubeConfigPath string) *cobra.Command {
 				}
 			}
 
-			// check if any certificates are currently being issued
+			fmt.Fprintf(os.Stdout, "Checking for certificates currently being issued...\n")
 			messagesIssuingCerts := []string{}
 			for _, cert := range certificates.Items {
 				issuing := false
@@ -127,7 +132,7 @@ func verify(run types.RunFunc, kubeConfigPath string) *cobra.Command {
 				}
 			}
 
-			// check for any potentially problematic certificate requests
+			fmt.Fprintf(os.Stdout, "Checking for stuck certificate requests...\n")
 			messagesCertificateRequests := []string{}
 			certificateRequestClient, err := clients.NewCertificateRequestClient(kubeCfg)
 			if err != nil {
@@ -167,9 +172,9 @@ func verify(run types.RunFunc, kubeConfigPath string) *cobra.Command {
 			messagesNextSteps := []string{}
 			if len(messagesOwnedSecrets) > 0 {
 				messagesNextSteps = append(messagesNextSteps, "Run 'jsctl experimental clusters cleanup secrets remove-certificate-owner-refs' to make sure secrets containing certificates are not garbage collected")
-				fmt.Fprintf(os.Stderr, "The following secrets contain certificates and are owned by a Certificate resource:\n")
+				fmt.Fprintf(os.Stdout, "The following secrets contain certificates and are owned by a Certificate resource:\n")
 				for _, message := range messagesOwnedSecrets {
-					fmt.Fprintf(os.Stderr, " * %s\n", message)
+					fmt.Fprintf(os.Stdout, " * %s\n", message)
 				}
 			}
 
@@ -180,34 +185,36 @@ func verify(run types.RunFunc, kubeConfigPath string) *cobra.Command {
 				}
 				messagesNextSteps = append(messagesNextSteps, fmt.Sprintf("Use cmctl to manually renew certificates: %s", strings.Join(cmctlRenewCmds, " && ")))
 
-				fmt.Fprintf(os.Stderr, "The following certificates will be renewed soon:\n")
+				fmt.Fprintf(os.Stdout, "The following certificates will be renewed soon:\n")
 				for _, message := range messagesUpcomingRenewals {
-					fmt.Fprintf(os.Stderr, " * %s\n", message)
+					fmt.Fprintf(os.Stdout, " * %s\n", message)
 				}
 			}
 
 			if len(messagesIssuingCerts) > 0 {
 				messagesNextSteps = append(messagesNextSteps, fmt.Sprintf("Wait for %d certificates to be issued", len(messagesIssuingCerts)))
-				fmt.Fprintf(os.Stderr, "The following certificates are currently being issued:\n")
+				fmt.Fprintf(os.Stdout, "The following certificates are currently being issued:\n")
 				for _, message := range messagesIssuingCerts {
-					fmt.Fprintf(os.Stderr, " * %s\n", message)
+					fmt.Fprintf(os.Stdout, " * %s\n", message)
 				}
 			}
 
 			if len(messagesCertificateRequests) > 0 {
 				messagesNextSteps = append(messagesNextSteps, fmt.Sprintf("Investigate %d pending certificate requests", len(messagesCertificateRequests)))
-				fmt.Fprintf(os.Stderr, "The following certificate requests are pending approval or issuance:\n")
+				fmt.Fprintf(os.Stdout, "The following certificate requests are pending approval or issuance:\n")
 				for _, message := range messagesCertificateRequests {
-					fmt.Fprintf(os.Stderr, " * %s\n", message)
+					fmt.Fprintf(os.Stdout, " * %s\n", message)
 				}
 			}
 
 			// print out any suggested next steps
 			if len(messagesNextSteps) > 0 {
-				fmt.Fprintf(os.Stderr, "\nSuggested next steps:\n")
+				fmt.Fprintf(os.Stdout, "\nSuggested next steps:\n")
 				for _, message := range messagesNextSteps {
-					fmt.Fprintf(os.Stderr, " * %s\n", message)
+					fmt.Fprintf(os.Stdout, " * %s\n", message)
 				}
+			} else {
+				fmt.Fprintf(os.Stdout, "\nNothing to do before uninstalling\n")
 			}
 
 			return nil
